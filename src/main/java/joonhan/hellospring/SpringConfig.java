99b3d22e8778348706f9454307b7908dc35e97ff
joonhyeok han <virq@naver.com>
2023-11-29 16:15:03 +0900

    feat: Add manual DI by Bean config

---
+++ src/main/java/joonhan/hellospring/SpringConfig.java
@@ -0,0 +1,21 @@
+package joonhan.hellospring;
+
+import joonhan.hellospring.repository.MemberRepository;
+import joonhan.hellospring.repository.MemoryMemberRepository;
+import joonhan.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
