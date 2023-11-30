c3e89db86a336118b443c2c532f5fef332d7f6da
joonhyeok han <virq@naver.com>
2023-11-30 15:37:28 +0900

    feat: Add JPA

--- src/main/java/joonhan/hellospring/SpringConfig.java
+++ src/main/java/joonhan/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package joonhan.hellospring;
 
+import joonhan.hellospring.repository.JpaMemberRepository;
 import joonhan.hellospring.repository.MemberRepository;
 import joonhan.hellospring.repository.MemoryMemberRepository;
 import joonhan.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
+
 @Configuration
 public class SpringConfig {
+
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+
+        return new JpaMemberRepository(em);
     }
 }
