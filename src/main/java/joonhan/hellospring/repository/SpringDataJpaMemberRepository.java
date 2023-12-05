21af36d98876d6e6f714ffa1a52d5e29e634088e
Joonhyeok Han <joonhyeok.han@gmail.com>
2023-12-05 15:12:19 +0900

    feat: Implement SpringDataJpaMemberRepository

---
+++ src/main/java/joonhan/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package joonhan.hellospring.repository;
+
+import joonhan.hellospring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
